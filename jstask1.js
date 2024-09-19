9e14d88b2bad4ea47d2644f6df0956e7a25bf7f4
chinnarao455 <dasarichinnarao455@gmail.com>
2024-09-19 12:21:26 +0530

    jstask1

---
+++ jstask1.js
@@ -0,0 +1,36 @@
+ var num=2
+ if(num>0){
+     console.log("positive")
+    if (num%2==0){
+        console.log("even")
+    }else{
+         console.log("odd")
+    }
+ }
+ else if(num<0){
+    console.log("negative")
+}
+else{
+     console.log("zero")
+}
+// 2nd programe
+
+ var n1=6
+var n2=4
+ if(n1%2==0 && n2%2==0){
+ console.log(n1+n2)
+ }else{
+    console.log("notvalid")
+}
+// num multiplies table
+
+var n=5
+var i=1
+for(i=1;i<=10;i++){
+    console.log(n+"*"+i+"="+n*i)
+ }
+    
+
+    
+
+
